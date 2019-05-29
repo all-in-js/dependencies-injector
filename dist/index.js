2530754a2b8fbb63e53a5713fd867a5a1de0496f
famanoder <915905174@qq.com>
2019-05-29 21:21:46 +0800

    a simple plan to do

--- dist/index.js
+++ dist/index.js
@@ -4,5 +4,21 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = Injectable;
+exports.UserService = void 0;
+
+var _class;
 
 function Injectable() {}
+
+let UserService = Injectable(_class = class UserService {
+  constructor(id) {
+    this.id = id;
+  }
+
+  getUserInfo(id) {
+    console.log('id');
+  }
+
+}) || _class;
+
+exports.UserService = UserService;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 export default function Injectable () {}
+
+@Inject('a', 'b')
+export class UserService {
+    constructor(a, b) {
+        this.id = id;
+    }
+    getUserInfo(id) {
+        console.log('id');
+    }
+}
+
+deps[init obj, add, remove], target => function(...deps) => constructor(...deps)
+
+Injector.resolve('a', 'b', function || constructor);
+const {a, b} = Injector.resolve('a', 'b');
+const [a, b] = Injector.resolve('a', 'b');
+
